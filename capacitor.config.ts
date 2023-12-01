6d756274bd4c7fcf7ecb4ff5a7d431fe3b40ffc2
Lucas Larangeira <luke@meuvidraceiro.com.br>
2023-12-01 05:59:34 -0300

    fix package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "com.lucaslarangeira.routinify",
   appName: "Routinify",
   webDir: "www",
   server: {
